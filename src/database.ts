fc162eb5f8fa9a8c37c7a1379dec58bbef1ae11c
gustavo <gustavo96ma@gmail.com>
2024-11-04 21:15:12 -0300

    sync

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('postgres', 'postgres', '123456', {
+export const sequelize = new Sequelize('postgres', 'postgres', 'postgres', {
     host: 'localhost',
     dialect: 'postgres',
 });
--- src/models/jogador.ts
+++ src/models/jogador.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../database";
 
@@ -27,32 +26,3 @@ Jogador.init(
      updatedAt: false,
     }
 );
-=======
-import { DataTypes, Model } from 'sequelize';
-import { sequelize } from '../database'; 
-
-export class Jogador extends Model {
-  public id!: number;
-  public nome!: string;
-}
-
-Jogador.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    nome: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    modelName: 'Jogador',
-    tableName: 'jogadores',
-    timestamps: false, 
-  }
-);
->>>>>>> 7caec4381cd20f8fc1072e0f9e3286a6d44a204a
